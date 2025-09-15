440dd23a1abdbd9e1d0ea2d5121371a987e986f1
hashimmalik59 <hm363050@gmail.com>
2025-09-15 12:02:40 +0500

    Hoisting var & functions

---
+++ Namaste JavaScript/03_hoistingVariables&Functions.js
@@ -0,0 +1,9 @@
+// Hoisting variables & functions
+
+// Hoisting => aik JS ka concept hai jo k Execution Contextk first phase, yani memory creation phase mein banta hai, jab variables aur functions memory phase mein top pe move hojate hai second phase yani execution phase se pehle
+
+// var hoist lekin => undefined
+
+// function decalration hoist lekin => poora execute hota hai
+
+// function expression with var hoist lekin => to memory phase mein undefined hai hota hai during memory allocation phase aur phir undefined call nahi hota, tabhi typeError: example is not a function
